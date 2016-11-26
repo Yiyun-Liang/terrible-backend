836e16b0ab268a782d62b0aa0441bbc194772b77
tangdian <dean99dean@gmail.com>
2016-11-26 12:32:23 -0500

    delete broadcast

--- public/js/controller.js
+++ public/js/controller.js
@@ -8,6 +8,7 @@ app.factory('mySocket', function (socketFactory) {
 
 app.controller("LeaderboardCtrl", function($scope, mySocket) {
 	mySocket.on("hello", function(data) {
+		console.log("AAAA");
 		console.log (data);
 		$scope.test = data;
 	});
