db91da6929c0f2c9150db90ab845b77c308b5dfb
tangdian <dean99dean@gmail.com>
2016-11-26 12:24:00 -0500

    change render to index html

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.sendFile(__dirname + '/index.html');
+// });
+
 router.get('/', function(req, res, next) {
-  res.sendFile(__dirname + '/index.html');
+   res.render('index');
 });
+
 
 module.exports = router;
